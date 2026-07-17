12a9062526a2d7eacc008bb71661580789175a4d
André <andrecjuniorr@gmai.com>
2026-07-16 21:41:50 -0300

    Trabalhando com records

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        var person = new Person("João");/*O age ele pegou e usou o do
+construtor secundario do Person, então conseguimos trabalhar com mais de 1
+construtor com record, porem somos obrigados a chamar o construtor default
+do record e por eles serem imutaveis ele obrigatoriamente precisam receber
+1 valor nem que seja um null em caso que eles aceitem. e o record é um
+recurso adcionado no java 17*/
+
+        System.out.println(person.getInfo());
+        System.out.println(person.name());
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,20 @@
-public record Person() {
+/*record não permite ser declaradas propriedades no corpo dele, só é
+permitido declarar atributos estaticos. e record é uma classe imutavel
+os valores dela não podem ser alterados, vc só pode usar ela para leitura
+ELA FOI FEITA PARA TRABALHAR COM OBJETOS IMUTAVEIS*/
+public record Person(String name, int age) {
+/*Também podemos trabalhar com métodosno record,mesmo ele tendo algumas
+limitações em relação as classes e tbm podemos ter construtores
+secundários */
+    public Person {
+
+    }
+
+    public Person (String name){
+        this(name, 1);
+    }
+
+    public String getInfo(){
+        return "Name: " + name + ", Age: " + age;
+    }
 }
